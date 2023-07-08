74e9076ed6c7c0b3e51f0e436742c8d45397ab27
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2023-07-08 16:53:42 +0200

    add "areMyFollowingScam" function

--- OSINT-IG/osint.js
+++ OSINT-IG/osint.js
@@ -1,6 +1,9 @@
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+// OTHER API TO IMPLEMENT:
+// https://www.instagram.com/api/v1/users/curiosity_astronomy/usernameinfo/ 
+
 exports.userInfo = (username) => {
 fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username=' + username, {
   method: 'GET',
@@ -38,7 +41,7 @@ fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username=' + usern
   }).catch((err) => console.log(err))
 }
 
-exports.userFollowing = (username, count) => {
+exports.userFollowing = (username, count, max_id) => {
   fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username=' + username, {
   method: 'GET',
   headers: {
@@ -52,7 +55,7 @@ exports.userFollowing = (username, count) => {
     const usernameId = json.data.user.id;
 
     //returning "count" number of following accounts
-    fetch('  https://www.instagram.com/api/v1/friendships/' + usernameId + '/following/?count=' + count, {
+    fetch('  https://www.instagram.com/api/v1/friendships/' + usernameId + '/following/?count=' + count + '&max_id=' + max_id, {
     method: 'GET',
     headers: {
         'Content-type': 'application/json',
@@ -113,6 +116,43 @@ exports.userFeedText = (username, count) => {
       feedTexts.push(element.caption.text)
     });
     console.log(feedTexts)
+
+  }).catch((err) => console.log(err))
+}
+
+exports.areMyFollowingScam = (username, count, max_id) => {
+  fetch('https://i.instagram.com/api/v1/users/web_profile_info/?username=' + username, {
+  method: 'GET',
+  headers: {
+    'Content-type': 'application/json',
+    'cookie': 'mid=ZChSiQAEAAHxicI9jfH_55hItQVr; ig_did=A3DE7F3E-FFB8-4F46-A071-57A86E730C58; fbm_124024574287414=base_domain=.instagram.com; dpr=2.200000047683716; datr=sMCdZAqQ8zYPOAd6H3QG-rkC; oo=v1%7C3%3A1688392216; csrftoken=VItQNt8xZkz4Em2K5BuFkusT6bvapjNL; ds_user_id=60150778235; sessionid=60150778235%3AKEVNbAxaEl0HqP%3A8%3AAYd6gs3Lpdcv1WQQxoKZwpNfGbDOsxkVoOYyXXwxeA; rur="CLN\x2c5460150778235\x2c541720272815:01f7292f90884bad6ec794ee7a05969623782aa6226b6c7b06f1e702c7e1572ff3d280f8"',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
+  },
+})
+  .then((response) => response.json())
+  .then((json) => {
+    const usernameId = json.data.user.id;
+
+    //returning "count" number of following accounts
+    fetch('  https://www.instagram.com/api/v1/friendships/' + usernameId + '/following/?count=' + count + '&max_id=' + max_id, {
+    method: 'GET',
+    headers: {
+        'Content-type': 'application/json',
+        'cookie': 'mid=ZKb90AAEAAHeJHFiThh3YHbtXxFj; ig_did=E1C82320-9298-44E8-AC11-0B73FD6F2A4B; csrftoken=8jpjVzXIehbiWFoXmJWtWVDV1KqA4gf4; ds_user_id=60150778235; sessionid=60150778235%3A1mkfoRdfeVMIfV%3A27%3AAYfcGNpKvqJ0XqWyTKU1doUREaFBvdK2nWSu-qKrEA; dpr=2.200000047683716; rur="CLN\x2c5460150778235\x2c541720202385:01f7588c3129eb9765ff944a46f68d000cc6030e5acb76cbc4f81d0fad5933a3e4ce60d4"',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      const followingList = [];
+      json.users.forEach(element => {
+        followingList.push({
+          "username": element.username,
+          "isScam": element.is_possible_scammer,
+        })
+      });
+        console.log(followingList)
+    }).catch((err) => console.log(err))
 
   }).catch((err) => console.log(err))
 }
