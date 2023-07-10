cbc5ac8f1bf1f9b8fc4df42d67e395c0493a160b
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2023-07-10 12:39:15 +0200

    update "test"

--- OSINT-IG/test.js
+++ OSINT-IG/test.js
@@ -1,13 +1,19 @@
 const osint = require("./osint");
 
 // Informazioni principali del profilo Target:
-//osint.userInfo("curiosity_astronomy");
+//osint.userInfo("ciro.marrazzo");
 
-// Lista degli utenti seguiti dal Target:
-//osint.userFollowing("curiosity_astronomy", 2);
+// Lista degli utenti seguiti dal Target: (username, count, max_id - for pagination)
+//osint.userFollowing("curiosity_astronomy", 2, 0);
 
 // Lista degli utenti che seguono il Target:
 //osint.userFollowers("curiosity_astronomy", 2);
 
 // Lista delle descrizioni dei primi "count" post nel feed del Target:
-osint.userFeedText("curiosity_astronomy", 2)
+//osint.userFeedText("curiosity_astronomy", 6)
+
+// Lista delle "location" dei primi "count" post nel feed del Target:
+osint.userFeedLocations("ciro.marrazzo", 3)
+
+// Restituise la lista dei primi "count" following del Target e controlla se è un profilo "scam"
+//osint.areMyFollowingScam("curiosity_astronomy", 50, 0);
