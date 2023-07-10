186344939474afe94d1c3d2a9895e7c983ae90e2
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2023-07-10 12:38:27 +0200

    add "userFeedLocations" function

--- OSINT-IG/osint.js
+++ OSINT-IG/osint.js
@@ -106,6 +106,7 @@ exports.userFeedText = (username, count) => {
   method: 'GET',
   headers: {
     'Content-type': 'application/json',
+    'cookie': 'mid=ZKb90AAEAAHeJHFiThh3YHbtXxFj; ig_did=E1C82320-9298-44E8-AC11-0B73FD6F2A4B; csrftoken=8jpjVzXIehbiWFoXmJWtWVDV1KqA4gf4; ds_user_id=60150778235; sessionid=60150778235%3A1mkfoRdfeVMIfV%3A27%3AAYfcGNpKvqJ0XqWyTKU1doUREaFBvdK2nWSu-qKrEA; dpr=2.200000047683716; rur="CLN\x2c5460150778235\x2c541720202385:01f7588c3129eb9765ff944a46f68d000cc6030e5acb76cbc4f81d0fad5933a3e4ce60d4"',
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
   },
 })
@@ -116,6 +117,35 @@ exports.userFeedText = (username, count) => {
       feedTexts.push(element.caption.text)
     });
     console.log(feedTexts)
+
+  }).catch((err) => console.log(err))
+}
+
+exports.userFeedLocations = (username, count) => {
+  fetch('https://www.instagram.com/api/v1/feed/user/' + username + '/username/?count=' + count, {
+  method: 'GET',
+  headers: {
+    'Content-type': 'application/json',
+    'cookie': 'mid=ZKb90AAEAAHeJHFiThh3YHbtXxFj; ig_did=E1C82320-9298-44E8-AC11-0B73FD6F2A4B; csrftoken=8jpjVzXIehbiWFoXmJWtWVDV1KqA4gf4; ds_user_id=60150778235; sessionid=60150778235%3A1mkfoRdfeVMIfV%3A27%3AAYfcGNpKvqJ0XqWyTKU1doUREaFBvdK2nWSu-qKrEA; dpr=2.200000047683716; rur="CLN\x2c5460150778235\x2c541720202385:01f7588c3129eb9765ff944a46f68d000cc6030e5acb76cbc4f81d0fad5933a3e4ce60d4"',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
+  },
+})
+  .then((response) => response.json())
+  .then((json) => {
+    const feedLocations = [];
+    json.items.forEach(element => {
+      if (element.location) {
+        feedLocations.push({
+          "short_name": element.location.short_name,
+          "name": element.location.name,
+          "address": element.location.address,
+          "city": element.location.city,
+          "lat": element.location.lat,
+          "lng": element.location.lng,
+        })
+      }
+    });
+    console.log(feedLocations)
 
   }).catch((err) => console.log(err))
 }
