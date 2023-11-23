9be9160fe4f646a0a4af91112e2fd37989eb5e53
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2023-11-23 17:45:38 +0100

    UPDATE Web UI Version

---
+++ Best fit algorithm/Web UI/app.py
@@ -0,0 +1,82 @@
+import argparse
+import yaml
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for
+from tool_manager import ToolManager
+from tool_selector import ToolSelector
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index2.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_tool():
+    addResult = None
+    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'yaml_file' not in request.files:
+            #flash('No file part')
+            return redirect(request.url)
+        file = request.files['yaml_file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(request.url)
+        if file and file.filename.endswith(('.yaml', '.yml')):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join('uploads', filename)
+            file.save(file_path)
+            addResult = tool_manager.add_tool_from_yaml(file_path)
+            print(addResult)
+            
+            os.remove(file_path)
+    
+    return render_template('add.html', result=addResult)
+
+@app.route('/remove', methods=['GET', 'POST'])
+def remove_tool():
+    tools = tool_manager.get_tools();
+    removeResult = None;
+    
+    if request.method == 'POST':
+        remove_tool = request.form.get('remove_tool')
+        print(remove_tool)
+        removeResult = tool_manager.remove_tool(remove_tool)
+        tools = tool_manager.get_tools();
+
+    return render_template('remove.html', tools=tools, result=removeResult)
+
+@app.route('/select', methods=['GET', 'POST'])
+def select_tool():
+    data = None
+    output_parameters, input_parameters = tool_selector.get_parameters();
+    
+    if request.method == 'POST':
+        # Estrai i parametri dalla richiesta WebUI
+        platform = request.form.get('platform')
+        input_params = request.form.getlist('input')
+        capability_params = request.form.get('capability')
+        output_params = request.form.getlist('output')
+
+        # Costruisci un oggetto argparse.Namespace
+        args = argparse.Namespace(platform=platform, input=input_params, capability=capability_params, output=output_params)
+
+        # Esegui la query Neo4j con i parametri forniti
+        data = tool_selector.select_tool(args)
+
+    return render_template('select.html', data=data, output_parameters=output_parameters, input_parameters=input_parameters)
+
+if __name__ == '__main__':
+    uri = "bolt://localhost:7687"
+    username = "neo4j"
+    password = "password"
+
+    tool_manager = ToolManager(uri, username, password)
+    tool_selector = ToolSelector(tool_manager)
+    
+    app.run(debug=True)
---
+++ Best fit algorithm/Web UI/tool_manager.py
@@ -0,0 +1,73 @@
+from neo4j import GraphDatabase
+import yaml
+
+class ToolManager:
+    def __init__(self, uri, username, password):
+        self.uri = uri
+        self.username = username
+        self.password = password
+        self.driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
+
+    def close(self):
+        self.driver.close()
+        
+    def get_tools(self):
+        tools = []
+
+        with self.driver.session() as session:
+            query = "MATCH (t:Tool) RETURN DISTINCT t.name as output"
+            result = session.run(query)
+            tools = [record["output"] for record in result.data()]
+
+        return tools
+
+    #Add a tool to the database
+    def add_tool(self, tool_name, platform, input_params, output_params):
+        with self.driver.session() as session:
+            query = f"""MERGE (t:Tool {{name: '{tool_name}', platform: '{platform}'}})
+            FOREACH (input IN {input_params} | MERGE (i:Input {{name: input}}) MERGE (t)-[:USES]->(i))
+            FOREACH (output IN {output_params} | MERGE (o:Resource {{label: output}}) MERGE (t)-[:PRODUCES]->(o))
+            FOREACH (platform IN CASE WHEN NOT '{platform}' IS NULL THEN ['{platform}'] ELSE [] END | MERGE (p:Platform {{name: platform}}) MERGE (t)-[:RUNS_ON]->(p));
+            """
+            result = session.run(query)
+            
+            # Checks if there are modified nodes
+            records_modified = result.consume().counters.nodes_created
+            print(records_modified)
+            
+            if records_modified == 0:
+                print(f"{tool_name} already exists")
+                return False
+            else:
+                print(f"{tool_name} has been added!")
+                return True
+
+    #Add a tool to the database using a YAML file
+    def add_tool_from_yaml(self, yaml_file):
+        with open(yaml_file, 'r') as file:
+            data = yaml.safe_load(file)
+
+            tool_name = data.get('name')
+            platform = data.get('platform')
+            input_params = data.get('input', [])
+            output_params = data.get('output', [])
+
+            if tool_name and platform:
+                return self.add_tool(tool_name, platform, input_params, output_params)
+            else:
+                print("Invalid YAML file format. Please make sure 'name' and 'platform' are specified.")
+
+    def remove_tool(self, tool_name):
+        with self.driver.session() as session:
+            query = f"MATCH (t:Tool) WHERE t.name = '{tool_name}' DETACH DELETE t"
+            result = session.run(query)
+            
+            # Checks if there are modified nodes
+            records_modified = result.consume().counters.nodes_deleted
+            
+            if records_modified == 0:
+                print(f"{tool_name} not found")
+                return False
+            else:
+                print(f"{tool_name} has been deleted!")
+                return True
