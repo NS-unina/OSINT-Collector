f3dfb6bc37369252c15aa395e4bfbb3360234e95
Marco Tammaro <marcotammaro42@gmail.com>
2023-12-03 13:12:51 +0100

    Launcher updates
    Better input management with env file
    Improved usage description
    Removed interactive usage (useless)
    Improved errors management

--- Launcher/launcher.py
+++ Launcher/launcher.py
@@ -2,60 +2,72 @@
 
 import sys
 import subprocess
+import os
 
 tools = {
     1: 'instaloader'
 }
 
 def show_usage():
-    print("Usage: export INPUT=uninait && {} <tool>".format(sys.argv[0]))
-    print("Available tools:", ", ".join(tools.values()))
-    
+    print("Usage:")
+    print("    launcher.py <tool> -i <input>")
+    print("\nParameters:")
+    print("    <tool>: Choose a tool from options:", ", ".join(tools.values()))
+    print("    -i <input>: Specify the input as a string.")
+    print("\nExample:")
+    print("    python launcher.py instaloader -i uninait")
+    sys.exit(0)
 
-def launch_tool(tool):
+def show_error(error: str):
+    print("[LauncherError] {}".format(error))
+    sys.exit(1)
+
+def launch_tool(tool: str, input: str):
     try:
-        subprocess.run(['docker-compose', '-f', './{}/docker-compose.yml'.format(tool), 'up', '-d'], check=True)
+        env = os.environ.copy()
+        env["LAUNCHER_INPUT"] = input
+        result = subprocess.run(['docker-compose', '-f', './{}/docker-compose.yml'.format(tool), 'up',], check=True, env=env)
+        
+        if (result.returncode != 0):
+            show_error("Error processing {}".format(tool))
+        
+        # Manage Output
+
     except subprocess.CalledProcessError as e:
-        print(f"Error launching {tool}: {e}")
+        show_error("Error launching {}: {}".format(tool, e))
 
-def interactive_main():
-    print("Select the tool to launch:")
-    for key, value in tools.items():
-        print(f"[{key}] {value}")
+def main():
 
-    try:
-        tool_number = int(input("Enter the number of the tool: "))
-        selected_tool = tools.get(tool_number)
-
-        if selected_tool:
-            launch_tool(selected_tool)
-        else:
-            print("Invalid tool number. Please select a valid tool.")
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-
-def headless_main():
-    if len(sys.argv) < 2:
-        show_usage()
-        sys.exit(1)
-
-    # Primo argomento è l'immagine Docker
     image = sys.argv[1]
+    input_flag = sys.argv[2]
+    input_str = sys.argv[3]
+    
+    # Check if the tool is valid
+    if (image not in tools.values()):
+        show_error("Invalid tool")
 
+    # Check if the input flag is present
+    if input_flag != "-i":
+        show_error("Invalid input")
+    
+    # Launch tool
     if (image in tools.values()):
-        launch_tool(image)
+        launch_tool(image, input_str)
     else:
-        print("Invalid input. Please enter a valid tool.")
+        show_error("Invalid tool")
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+    # Check hor help
+    if len(sys.argv) == 2:
+        help_flag = ["--help", "-h"]
+        help_input = sys.argv[1]
+        if help_input in help_flag:
             show_usage()
-            sys.exit(0)
 
-    if len(sys.argv) == 1:
-        interactive_main()
-    else:
-        headless_main()
+    # Check if all args have been provided
+    if len(sys.argv) != 4:
+        show_error("Invalid format")
+
+    main()
