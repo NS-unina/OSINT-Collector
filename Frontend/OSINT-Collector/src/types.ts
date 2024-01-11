05e06785540ad047091754c73ce62ee06bcc8bea
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-01-11 13:26:16 +0100

    ADD types

---
+++ Frontend/OSINT-Collector/src/types.ts
@@ -0,0 +1,20 @@
+// types.ts
+export interface Capability {
+    id: number;
+    name: string;
+  }
+  
+export interface ServerResponse {
+    capability_parameters: string[];
+}
+
+export interface RequiredInput {
+    input: {
+        label: string;
+        name: string;
+    }[];
+    tool: {
+        name: string;
+        platform: string;
+    };
+}
