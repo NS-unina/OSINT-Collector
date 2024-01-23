3a8292362cbb8892959ea56c956355b05e2182b6
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-01-23 17:11:21 +0100

    ADD test for python code execution

--- Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/ToolController.java
+++ Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/ToolController.java
@@ -10,6 +10,7 @@ import reactor.core.publisher.Flux;
 import org.yaml.snakeyaml.Yaml;
 import reactor.core.publisher.Mono;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -70,8 +71,43 @@ public class ToolController {
     }
 
     @PostMapping("/run")
-    public void runTools(@RequestBody List<Map<String, Object>> toolsInputs) {
-        System.out.print(toolsInputs);
+    public boolean runTools(@RequestBody String toolsInputs) {
+
+        // Test to verify execution of Python code
+
+        try {
+            // Specify the path to the Python script
+            String scriptPath = "/Users/ciromarrazzo/OSINT/OSINT-Collector/test.py";
+
+            // Set up the process builder for executing the Python script
+            ProcessBuilder processBuilder = new ProcessBuilder("python3.7", scriptPath, toolsInputs);
+
+            // Set the working directory for the process to the location of the Python script
+            processBuilder.directory(new File("/Users/ciromarrazzo/OSINT/OSINT-Collector/"));
+
+            // Start the external process (Python script)
+            Process process = processBuilder.start();
+
+            // Capture and print the output of the Python script
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Wait for the completion of the Python script execution
+            int exitCode = process.waitFor();
+
+            // Return true if the Python script executed successfully (exit code 0)
+            return exitCode == 0;
+        } catch (IOException | InterruptedException e) {
+            // Print stack trace in case of IOException or InterruptedException
+            e.printStackTrace();
+
+            // Return false if an exception occurs during the execution of the Python script
+            return false;
+        }
     }
 
 }
