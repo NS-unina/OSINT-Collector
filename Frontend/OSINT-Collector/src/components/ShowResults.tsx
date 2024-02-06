45d58b7b0dc3a9f7be98c0d1386df6029d633f15
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-02-06 16:56:41 +0100

    UPDATE Frontend/OSINT-Collector/src/components/ShowResults.tsx

--- Frontend/OSINT-Collector/src/components/ShowResults.tsx
+++ Frontend/OSINT-Collector/src/components/ShowResults.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Launch } from "../types";
 import { blackbird, instaloader } from "../types/results";
-
-import { MdOpenInNew } from "react-icons/md";
+import BlackbirdResults from "./BlackbirdResults";
 
 interface Props {
   selectedLaunch: Launch | null;
@@ -53,33 +52,7 @@ const ShowResults = ({ selectedLaunch }: Props) => {
   switch (selectedLaunch.image) {
     case "blackbird":
       if (results !== null && "sites" in results) {
-        return (
-          <div>
-            <div className="row mt-3">
-              {results.sites
-                .sort((a, b) => a.id - b.id)
-                .map((site) => (
-                  <div key={site.id} className="col-sm-3 mb-3 mb-sm-3">
-                    <a
-                      href={site.url}
-                      target="_blank"
-                      className="card-link position-relative"
-                    >
-                      <div className="card h-100">
-                        <div className="card-body">
-                          <h5 className="card-title">{site.site}</h5>
-                          <h6 className="card-subtitle mb-2 text-body-secondary">
-                            {results.username}
-                          </h6>
-                        </div>
-                        <MdOpenInNew className="position-absolute top-0 end-0 mt-2 me-2 invisible" />
-                      </div>
-                    </a>
-                  </div>
-                ))}
-            </div>
-          </div>
-        );
+        return <BlackbirdResults results={results} />;
       }
       break;
     case "instaloader":
