74407bea99a657a64cb2bdf528e1e0a4f4cb0d4b
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-02-10 15:57:37 +0100

    UPDATE ToolRepository.java.

--- Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/repository/ToolRepository.java
+++ Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/repository/ToolRepository.java
@@ -19,28 +19,33 @@ public interface ToolRepository extends ReactiveNeo4jRepository<Tool, String> {
     Mono<Void> deleteTool(String toolName);
 
     @Query("""
-            MATCH (t:Tool)-[:HAS_CAPABILITY]->(c:Capability)-[:NEEDS]->(i:Resource)
+            MATCH (t:Tool)-[:HAS_CAPABILITY]->(c:Capability)-[:NEEDS]->(i)
             WHERE ANY(substring IN $capabilities WHERE c.name CONTAINS substring)
             RETURN COLLECT(DISTINCT properties(t)) as tool, COLLECT(DISTINCT properties(i)) as inputs, c as capability
             """)
     Flux<ToolInput> getRequiredInputs(List<String> capabilities);
 
     @Query("""
-    MERGE (t:Tool {name: $toolName, platform: $platform})
-    MERGE (p:Platform {name: $platform})
-    MERGE (t)-[:RUNS_ON]->(p)
-    WITH t, p
-    UNWIND $capabilities AS cap
-    MERGE (c:Capability {name: cap.name, description: cap.description})
-    MERGE (t)-[:HAS_CAPABILITY]->(c)
-    WITH c, t, cap, cap.inputs AS inputs
-    UNWIND inputs AS input
-    MATCH (i:Resource {name: input})-[*]->(r:Resource {name: $platform})
-    FOREACH (ignore IN CASE WHEN i IS NOT NULL THEN [1] ELSE [] END | MERGE (c)-[:NEEDS]->(i))
-    WITH c, t, cap, cap.outputs AS outputs
-    UNWIND outputs AS output
-    MATCH (o:Resource {name: output})-[*]->(:Resource {name: $platform})
-    FOREACH (ignore IN CASE WHEN o IS NOT NULL THEN [1] ELSE [] END | MERGE (c)-[:PRODUCES]->(o))
+     MERGE (t:Tool {name: $toolName, platform: $platform})
+     MERGE (p:Platform {name: $platform})
+     MERGE (t)-[:RUNS_ON]->(p)
+     WITH t, p
+     UNWIND $capabilities AS cap
+     MERGE (c:Capability {name: cap.name, description: cap.description})
+     MERGE (t)-[:HAS_CAPABILITY]->(c)
+     WITH c, t, cap, cap.inputs AS inputs
+     UNWIND inputs AS input
+     OPTIONAL MATCH (i:Resource {name: input})-[*]->(r:Resource {name: $platform})
+     WITH c, t, cap, i, input
+     FOREACH (_ IN CASE WHEN i IS NULL THEN [1] ELSE [] END |\s
+         MERGE (i:Input {name: input})
+         MERGE (c)-[:NEEDS]->(i))
+     FOREACH (_ IN CASE WHEN i IS NOT NULL THEN [1] ELSE [] END |\s
+         MERGE (c)-[:NEEDS]->(i))
+     WITH c, t, cap, cap.outputs AS outputs
+     UNWIND outputs AS output
+     MATCH (o:Resource {name: output})-[*]->(:Resource {name: $platform})
+     FOREACH (ignore IN CASE WHEN o IS NOT NULL THEN [1] ELSE [] END | MERGE (c)-[:PRODUCES]->(o))                                                                                     \s
     """)
     Mono<Void> addTool(String toolName, String platform, List<Map<String, Object>> capabilities);
 
