8c8ae5ecb78e89118aa0a1ca8bacc578dd1101f0
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-02-17 12:10:11 +0100

    UPDATE LogstashController

--- Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/LogstashController.java
+++ Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/LogstashController.java
@@ -1,10 +1,11 @@
 package com.unina.osintcollector.controller;
 
+import com.unina.osintcollector.model.TelegramPost;
 import com.unina.osintcollector.repository.TelegramPostRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("logstash")
@@ -17,34 +18,29 @@ public class LogstashController {
     }
 
     @PostMapping("/snscrape-telegram")
-    public String getSnscrapeResults(@RequestBody String post) {
+    public Mono<ResponseEntity<Object>> getSnscrapeResults(@RequestBody TelegramPost post) {
 
+        String[] parts = post.getUrl().split("/");
+        String channelName = parts[4];
+
+        System.out.println("DATA RECEIVED");
         System.out.println(post);
-        return "OK";
 
-//        String[] parts = post.getUrl().split("/");
-//        String channelName = parts[4];
-//
-//        System.out.println("DATA RECEIVED");
-//        System.out.println(post);
-//
-//        return telegramPostRepository.saveChannelAndPost(channelName, post.getUrl(), post.getText());
+        return telegramPostRepository.saveChannelAndPost(channelName, post.getUrl(), post.getText(), post.getDate())
+                .thenReturn(ResponseEntity.status(HttpStatus.OK).build());
     }
 
     @PostMapping("/instaloader")
-    public String getInstaloaderResults(@RequestBody String post) {
-
+    public ResponseEntity<String> getInstaloaderResults(@RequestBody String post) {
         System.out.println(post);
-        return "OK";
-
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("/blackbird")
-    public String getBlackbirdResults(@RequestBody String post) {
+    public ResponseEntity<Object> getBlackbirdResults(@RequestBody String site) {
 
-        System.out.println(post);
-        return "OK";
-
+        System.out.println(site);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
