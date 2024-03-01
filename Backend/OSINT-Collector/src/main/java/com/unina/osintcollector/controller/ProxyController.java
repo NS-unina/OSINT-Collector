d93a5df3cd1855fde14c67327c3957ed33034c28
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2024-03-01 13:56:23 +0100

    UPDATE telegram search API url

--- Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/ProxyController.java
+++ Backend/OSINT-Collector/src/main/java/com/unina/osintcollector/controller/ProxyController.java
@@ -17,7 +17,7 @@ public class ProxyController {
     public String proxyGoogleCSE(@RequestParam String param, String page) throws IOException {
         String encodedParam = URLEncoder.encode(param, StandardCharsets.UTF_8);
 
-        URL url = new URL("https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&hl=en&source=gcsc&gss=.io&start=" + page + "&cselibv=8435450f13508ca1&cx=006249643689853114236%3Aa3iibfpwexa&q=" + encodedParam + "&safe=off&cse_tok=AB-tC_59DoiauQdHWNJNzoowt1m3%3A1709059422192&sort=&exp=cc%2Cdtsq-3&fexp=72497452&callback=google.search.cse.api10114");
+        URL url = new URL("https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&hl=en&source=gcsc&gss=.io&start=" + page + "&cselibv=8435450f13508ca1&cx=006249643689853114236%3Aa3iibfpwexa&q=" + encodedParam + "&safe=off&cse_tok=AB-tC_62wzvqDa9Xc_l1nskMfTS_%3A1709231891652&sort=&exp=cc%2Cdtsq-3&fexp=72497452&oq=" + encodedParam + "&gs_l=partner-generic.12..0.13284.14163.0.18746.7.7.0.0.0.0.116.627.6j1.7.0.csems%2Cnrl%3D10...0....1.34.partner-generic..0.7.626.XsvdQyj4h7w&callback=google.search.cse.api6762");
 
         HttpURLConnection http = (HttpURLConnection) url.openConnection();
 
