4d366df8d45ce3ab5b44de1b9aeb68d0b9027523
NdA994 <nda994@gmail.com>
2025-10-05 17:40:48 +0200

    ref p.2

--- Laucher/app/__init__.py
+++ Laucher/app/__init__.py
@@ -8,15 +8,17 @@ def create_app(config_class="app.configuration.DevelopmentConfig"):
     Application factory function.
     Allows creating multiple Flask app instances with different configs.
     """
+    
     app = Flask(__name__)
     app.config.from_object(config_class)
-
     app.register_blueprint(views_bp)
 
     return app
 
 
 if __name__ == "__main__":
+
+
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
