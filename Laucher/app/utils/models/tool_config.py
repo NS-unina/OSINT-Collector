f7086e559d76783f699a75f10fec3f8be4c4f471
NdA994 <nda994@gmail.com>
2025-10-10 22:50:14 +0200

    add models

--- Laucher/app/utils/models/tool_config.py
+++ Laucher/app/utils/models/tool_config.py
@@ -1,16 +1,21 @@
 """class for model tool"""
+from typing import List
+from pathlib import Path
+import yaml
 
 class Input:
     input_key: str
     description: str
     type: str
 
-
     def __init__(self, input_key: str, description: str, type: str):
         self.input_key = input_key
         self.description = description
         self.type = type
 
+    def __repr__(self):
+        return f"Input(key={self.input_key}, type={self.type})"
+    
 
 class EntrypointConfig:
     feature_key: str
@@ -19,11 +24,15 @@ class EntrypointConfig:
     inputList: list[Input]
 
 
-    def __init__(self, feature_key: str, name: str, description: str, inputList: list[Input]):
+    def __init__(self, feature_key: str, name: str, description: str, command: str, inputList: list[Input]):
         self.feature_key = feature_key
         self.name = name
         self.description = description
+        self.command = command
         self.inputList = inputList
+
+    def __repr__(self):
+        return f"Entrypoint(feature={self.feature_key}, inputs={[i.input_key for i in self.inputList]})"
 
 
 class ToolConfig:
@@ -34,11 +43,39 @@ class ToolConfig:
     entrypointList: list[EntrypointConfig]
 
 
-    def __init__(self, toolName: str, description: str, image: str.type, type: str, entrypointList: list[EntrypointConfig]):
+    def __init__(self, toolName: str, description: str, image: str, entrypointList: list[EntrypointConfig]):
         self.toolName = toolName
         self.description = description
         self.image = image
-        self.type = type
         self.entrypointList = entrypointList
 
+    def __init__(self, tool_config: dict):
+        self.toolName = next(iter(tool_config))
+        tool_data = tool_config[self.toolName]
+
+        self.description = tool_data.get("description", "")
+        self.image = tool_data.get("image", "")
+        self.entrypointList: List[EntrypointConfig] = []
+
+        for ep in tool_data.get("entrypoints", []):
+            entry_inputs = []
+
+            for input_key in ep.get("inputs", []):
+                input_def = next((i for i in tool_data["inputs"] if i["key"] == input_key), None)
+                if input_def:
+                    entry_inputs.append(Input(input_def["key"], input_def["description"], input_def["type"]))
+                else:
+                    print(f" Warning: input '{input_key}' not defined in tool inputs.")
+
+            entry = EntrypointConfig(
+                feature_key=ep["feature_key"],
+                name=ep["name"],
+                description=ep["description"],
+                command=ep["command"],
+                inputList=entry_inputs)
+
+            self.entrypointList.append(entry)
+
+    def __repr__(self):
+        return f"ToolConfig(name={self.toolName}, entrypoints={self.entrypointList})"
 
--- Laucher/app/views.py
+++ Laucher/app/views.py
@@ -1,3 +1,4 @@
+from app.utils.models.tool_config import ToolConfig
 from app.utils.services.tool_manager import ToolManager
 from flask import Blueprint, request, jsonify
 import yaml
@@ -12,7 +13,7 @@ def tool_details_help(tool: str):
     Gives the help message for a specific tool
     """
     try:
-        tool_config = ToolManager.read_tool_config(tool)    
+        tool_config = ToolManager.read_tool_config(tool)
         return tool_config, 200
     except FileNotFoundError:
         return jsonify({"error": f"Configuration file for tool '{tool}' not found."}), 404
@@ -28,15 +29,27 @@ def tools_help():
     return ToolManager.tools_list(), 200
 
 
+# curl -X POST http://127.0.0.1:5000/launch -H "Content-Type: application/json" -d '{"tool": "instaloader", "feature_key": "download-public-profile", "inputs": {"CHANNEL": "ciccio"}}'
 @views_bp.route("/launch", methods=['POST'])
 def launch():
     """
     Launcher
     """
-    
-    print(request.json)
 
-    return "<p>It Works!</p>"
+    tool_image = request.json["tool"] 
+    feature_key = request.json["feature_key"]
+    inputs = request.json["feature_key"]
+
+    try:
+        dict_tool = ToolManager.read_tool_config(tool_image)
+        tool_config = ToolConfig(dict_tool)
+        print(tool_config.toolName)
+        # print(tool_config[tool_image]["entrypoints"][0])
+        return "<p>It Works!</p>", 200
+    except FileNotFoundError:
+        return jsonify({"error": f"Configuration file for tool '{tool_image}' not found."}), 404
+    except yaml.YAMLError:
+        return jsonify({"error": f"Configuration file for tool '{tool_image}' is invalid."}), 500
 
 
 @views_bp.route("/")
